f8f882afd93868ed8ccc64534fed1bca8081c9be
BluePawDev <jaruby@me.com>
2017-02-08 11:03:03 -0600

    Update ng- to data-ng-; add var myApp

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+var myApp = angular.module('myApp', []);
+
+
+
+
+
 // MODULE
-var angularApp = angular.module('angularApp', []);
+// var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    
-}]);
-
-
-
+// angularApp.controller('mainController', ['$scope', function ($scope) {
+//
+// }]);
